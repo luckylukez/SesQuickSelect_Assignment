d878fc0777e05c20eb3d143f91226b770920e1cd
Lukas Johansson <lukas.g.johansson@gmail.com>
2023-11-08 18:02:07 +0100

    First working algorithm version + simple test

---
+++ assigment-2.py
@@ -0,0 +1,129 @@
+import numpy as np
+import random
+
+# Create data
+T = 10 # nr of arrays
+n = 30000 # size of each array
+
+data = []
+for i in range(1,T):
+    row = list(range(1,n))
+    random.shuffle(row)
+    data.append(row)
+data = np.matrix(data)
+
+random.seed(1)
+
+def partition(arr, lo, hi, piv_ind):
+    p = arr[piv_ind]
+    if piv_ind == hi:
+        hi -=1
+    elif piv_ind == lo:
+        lo += 1
+    
+    i = lo
+
+    for j in range(lo, hi+1): 
+          
+        if arr[j] <= p: 
+            arr[i], arr[j] = arr[j], arr[i] 
+            i += 1
+    
+    # if the pivot is the low one, change i to index with value under p
+    if piv_ind < lo:
+        i -= 1
+              
+    arr[i], arr[piv_ind] = arr[piv_ind], arr[i]
+    return i
+
+def YBB_partition(arr, lo, hi):
+    # p is the left pivot, and q is the right pivot. 
+    j = k = lo + 1
+    g, p, q = hi - 1, arr[lo], arr[hi] 
+    while k <= g: 
+
+        # If elements are less than the left pivot 
+        if arr[k] < p: 
+            arr[k], arr[j] = arr[j], arr[k] 
+            j += 1
+            # print(3,arr[lo:hi+1])
+        # If elements are greater than or equal  
+        # to the right pivot 
+        elif arr[k] >= q: 
+            while arr[g] > q and k < g: 
+                g -= 1
+                  
+            arr[k], arr[g] = arr[g], arr[k] 
+            g -= 1
+            # print(4,arr[lo:hi+1])
+              
+            if arr[k] < p: 
+                arr[k], arr[j] = arr[j], arr[k] 
+                j += 1
+                # print(5,arr[lo:hi+1]) 
+                  
+        k += 1
+          
+    j -= 1
+    g += 1
+    # Bring pivots to their appropriate positions. 
+    arr[lo], arr[j] = arr[j], arr[lo] 
+    arr[hi], arr[g] = arr[g], arr[hi] 
+
+    # Returning the indices of the pivots 
+    return j, g
+
+def sesquickselect(arr, k, lo, hi, v=1/4):
+
+    if arr[lo] > arr[hi]: 
+        arr[lo], arr[hi] = arr[hi], arr[lo]
+
+    i = k - lo # rank of k in sub-array
+    n = hi - lo + 1
+    alpha = i/n
+
+    # Since k is rank (starts at 1) but will be caompared to indexes (starts at 0) we subtract one
+    k_ind = k-1
+
+    if alpha >= v and alpha <= 1-v:
+        lo_p, hi_p = YBB_partition(arr, lo, hi)
+
+        if k_ind == lo_p or k_ind == hi_p:
+            return arr[k_ind]
+        elif k_ind < lo_p:
+            return sesquickselect(arr, k, lo, lo_p-1, v)
+        elif k_ind < hi_p:
+            return sesquickselect(arr, k, lo_p+1, hi_p-1, v)
+        else:
+            return sesquickselect(arr, k, hi_p+1, hi, v)
+
+
+    else:
+        if alpha < v:
+            piv_ind = lo
+        elif alpha > 1-v:
+            piv_ind = hi
+
+        p = partition(arr, lo, hi, piv_ind=piv_ind)
+
+        if k_ind == p: 
+            return arr[p]
+        elif k_ind < p:
+            return sesquickselect(arr, k, lo, p-1)
+        else:
+            return sesquickselect(arr, k, p+1, hi)
+    
+# Simple test
+for i in range(3):
+    print(i)
+n = 10
+a = list(range(n))
+random.shuffle(a)
+print(a)
+for k in range(1,n+1):
+    random.shuffle(a)
+    print(sesquickselect(a, k, 0,len(a)-1))
+
+
+
+
